ab651c1da0a886d5c77f2674786611de972eaf75
zha779645641TeTEst <zha779645641@outlook.com>
2019-05-02 19:11:48 +0800

    自定义验证器

--- ClientApp/src/app/account/account.component.ts
+++ ClientApp/src/app/account/account.component.ts
@@ -1,68 +1,69 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Account } from '../account';
-import { AuthenticationService } from '../authentication.service';
-import { first } from 'rxjs/operators';
-import { Router, Route, ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { invalidAccountValidator } from '../../Shared/invalidAccount.directive';
+  import { Component, OnInit, Input } from '@angular/core';
+  import { Account } from '../account';
+  import { AuthenticationService } from '../authentication.service';
+  import { first } from 'rxjs/operators';
+  import { Router, Route, ActivatedRoute } from '@angular/router';
+  import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+  import { invalidAccountValidator } from '../../Shared/invalidAccount.directive';
 
-@Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss'],
-})
-export class AccountComponent implements OnInit {
-  returnUrl: '';
-  error: string;
-  submitForm = false;
-  submitted = false;
-  account: Account = { account: "", password: "" }
-
-  constructor(
-    private fb: FormBuilder,
-    private router: Router, 
-    private route: ActivatedRoute,
-    private authenticationService: AuthenticationService
-  ) { }
-
-  loginForm: FormGroup = this.fb.group({
-    account: ['', invalidAccountValidator()],
-    password: ['', [Validators.required, Validators.minLength(4)]],
+  @Component({
+    selector: 'app-account',
+    templateUrl: './account.component.html',
+    styleUrls: ['./account.component.scss'],
   })
-  ngOnInit() {
-    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-    this.authenticationService.logout();
-  }
+  export class AccountComponent implements OnInit {
+    returnUrl: '';
+    error: string;
+    submitForm = false;
+    submitted = false;
+    account: Account = { account: "", password: "" }
 
-  get formData() {
-    return this.loginForm.controls;
-  }
+    constructor(
+      private fb: FormBuilder,
+      private router: Router,
+      private route: ActivatedRoute,
+      private authenticationService: AuthenticationService
+    ) { }
 
-  get formAccount() {
-    return this.loginForm.get('account');
-  }
+    loginForm: FormGroup = this.fb.group({
+      account: ['', invalidAccountValidator()],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    })
+    ngOnInit() {
+      this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+      this.authenticationService.logout();
+    }
 
-  get formPassword() {
-    return this.loginForm.get('password');
-  }
+    get formData() {
+      return this.loginForm.controls;
+    }
 
-  onLogin() {
-    if (this.loginForm.valid) {
-      this.submitForm = true;
-      this.submitted = true;
-      this.account.account = this.formData.account.value;
-      this.account.password = this.formData.password.value;
-      this.authenticationService.login(this.account)
-        .pipe(first())
-        .subscribe(
-          data => {
-            this.router.navigate([this.returnUrl]);
-          },
-          error => {
-            this.error = error;
-            this.submitForm = false;
-          }
-        );
+    get formAccount() {
+      return this.loginForm.get('account');
+    }
+
+    get formPassword() {
+      return this.loginForm.get('password');
+    }
+
+    onLogin() {
+      if (this.loginForm.valid) {
+        this.submitForm = true;
+        this.submitted = true;
+        this.account.account = this.formData.account.value;
+        this.account.password = this.formData.password.value;
+        this.error=null;
+        this.authenticationService.login(this.account)
+          .pipe(first())
+          .subscribe(
+            data => {
+              this.router.navigate([this.returnUrl]);
+            },
+            error => {
+              this.error = error;
+              this.submitForm = false;
+            }
+          );
+      }
     }
   }
-}
